ebec3f521a899cb92ab6caa57ec4e71953721dea
BasnakEA <BasnakYA@mpei.ru>
2020-04-10 11:50:43 +0300

    module testing fing_minmax

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+#include "histogram.h"
 using namespace std;
 
 vector<double> input_numbers(const size_t count) {
@@ -13,18 +13,7 @@ vector<double> input_numbers(const size_t count) {
     return result;
 }
 
-void find_minmax (const vector<double>& numbers, double& min, double& max) {
-    min = numbers[0];
-    max = numbers[0];
-    for (double number : numbers) {
-        if (number < min) {
-            min = number;
-        }
-        if (number > max) {
-            max = number;
-        }
-    }
-}
+
 
 vector<size_t> make_histogram(const vector<double>& numbers, const size_t count) {
     vector<size_t> result(count);
