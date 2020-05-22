474280097a95a2fc4a70c817918f29957aa948a8
BasnakEA <BasnakYA@mpei.ru>
2020-05-22 10:13:10 +0300

    Revert "code: tested printf" This reverts commit b0fe1ecbb6a88e9bd8e25ff338d69492257eb500.

--- histogram.h
+++ histogram.h
@@ -4,15 +4,6 @@
 #include<vector>
 using namespace std;
 
-struct Input {
-    vector<double> numbers;
-    size_t bin_count;
-};
-
 void find_minmax (const vector<double>& numbers, double& min, double& max);
 
-vector<size_t> make_histogram(const Input& data);
-
-void show_histogram_text(vector<size_t> bins);
-
-#endif // HISTOGRAM_H_INCLUDED
+#endif
